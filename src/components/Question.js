1b949620cc599dc800f286f0616ee9bd431d2476
Tumaini <tumainikebaya.234@gmail.com>
2024-06-11 19:34:25 +0300

    Adds Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() =>{
+    const timeID = setTimeout(() => {
+      setTimeRemaining((time) => time - 1);
+    }, 1000);
+
+    return function cleanup() {
+      clearTimeout(timeID);
+    };
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
